d99f90e1fe3918a0d0a93c412318e3e1185d98e3
gitaunthama <gitaunthama@gmail.com>
2024-03-19 14:32:14 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+// const name = "Joe";
+// const height = 74;
+// const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+// module.exports = {name, height, message};
 
-console.log(message);
+// console.log(message);
+
+//   function sayHelloToLiz()
+//  {
+//     console.log("Hello, Liz!");
+//   }
+//   sayHelloToLiz();
+
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function sayHiToHeadphonedRoommate()
+ {
+    console.log("Hi, HeadphonedRoommate!");
+  }
+  sayHiToHeadphonedRoommate();
--- test/indexTest.js
+++ test/indexTest.js
@@ -36,3 +36,9 @@ describe('', () => {
     })
   })
 })
+describe("shout(string)", function () {
+  it("receives one argument and returns it in all caps", function () {
+    // we'll get to this in a sec
+  });
+});
+
